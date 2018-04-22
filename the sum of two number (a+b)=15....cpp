0f77858b5b4d0813c21b78add680c3b94ebd2bea
hummyboy <38236662+hummyboy@users.noreply.github.com>
2018-04-22 14:17:05 +0545

    Add files via upload

---
+++ the sum of two number (a+b)=15....cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,sum; 
+	a=5;
+	b=10;
+	printf("enter the value of a and b");
+	scanf("%d%d",&a,&b);
+	sum=(a+b);
+	printf("sum of two number %d%d is %d",a,b,sum);
+	getch();
+}
+	
+	
