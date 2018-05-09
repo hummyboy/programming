10b83a0dcb1a5e10323d6fc13d0d859ff016870a
hummyboy <38236662+hummyboy@users.noreply.github.com>
2018-05-09 14:33:36 +0545

    absolute value

---
+++ absolute value.cpp
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float r,area;
+	printf("Enter the value of radius:\n");
+	scanf("%f",&r);
+	area=3.14*r*r;
+	printf("Area of circle is %.2f",area);
+	getch();
+}
