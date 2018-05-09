0e8ced1115aee79010325dcf13838ec99d026ca1
hummyboy <38236662+hummyboy@users.noreply.github.com>
2018-05-09 19:01:03 +0545

    Add files via upload

---
+++ area of triangek is right angle triangle.cpp
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float height,breath,area;
+	height=30;
+	breath=15;
+	area=(height*breath)/2;
+	printf("Area of a triangle is %f",area);
+	getch();
+}
