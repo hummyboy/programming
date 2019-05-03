de57565379df48ccbc136a731eee2bf93d948d20
hummyboy <38236662+hummyboy@users.noreply.github.com>
2019-05-03 09:02:55 +0545

    scalar traingle

---
+++ scaler triangle.cpp
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of a,b,c:\n");
+	scanf("%d%d%d",&a,&b,&c);
+	if((a|=b)&&(b|=c))
+	{
+		printf("Triangle is scalar");
+	}
+	else
+	{
+		printf("Triangle is scalar");
+	}
+	getch();
+}
