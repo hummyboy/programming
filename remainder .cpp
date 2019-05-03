1333788297f6146ca2dbb20b87f4336d52beaede
hummyboy <38236662+hummyboy@users.noreply.github.com>
2019-05-03 09:01:36 +0545

    remainder calculation

---
+++ remainder .cpp
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,remainder;
+	printf("enter value of a:");
+	scanf("%d",&a);
+	printf("entervalue of b:");
+	scanf("%d",&b);
+	remainder=a%b;
+	printf("reminder is %d",remainder);
+	getch();
+	
+}
