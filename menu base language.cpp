2501eb02e88981ba88a03e500d8a454301561722
hummyboy <38236662+hummyboy@users.noreply.github.com>
2019-05-03 08:57:25 +0545

    menu based

---
+++ menu base language.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int age;
+    printf("enter the age ");
+    scanf("%d",&age);
+    if (age <18)
+    {
+      pritf("sorry you cant vote.you are under age");
+      {
+      	else (age>18)
+      	printf("welcome you can vote in election");
+      	getch();
+	  }
+    
